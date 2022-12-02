c6497bb082cf834c01fee736523694acb4b62ad4
UT-03 <f20200458@pilani.bits-pilani.ac.in>
2022-12-02 18:56:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <React.Fragment>
+      <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a new para...
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
